843e865f7d6cc68096239b23d7587524bd52caca
GeovaniPossenti <possentigeovani@gmail.com>
2021-04-11 03:37:59 -0300

    commit change css

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="tools/css/style.css">
+    <link rel="stylesheet" href="tools/css/styleindex.css">
     <!--Bootstrap.-->
     <script src="tools/lib/bootstrap-5.0.0-beta3-dist/js/bootstrap.min.js"></script>
     <link rel="stylesheet" type="text/css" href="tools/lib/bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css">
