094b16886798923e6c5b9d86d193da8973322156
GeovaniPossenti <possentigeovani@gmail.com>
2021-04-28 22:45:07 -0300

    Style relatorio

--- view/relatorio.php
+++ view/relatorio.php
@@ -48,15 +48,15 @@
             $pdf->SetFillColor(255,255,255);
             
             //Número da ordem de serviço.
-            $pdf->SetFont('OpenSansLight','',8);
-            $pdf->SetY(1); $pdf->SetX(10);
-            $pdf->Cell(18,8, utf8_decode("Ordem de serviço N°: $id_os"),0,0,"C","false");
+            $pdf->SetFont('OpenSansLight','',10);
+            $pdf->SetY(1); $pdf->SetX(15);
+            $pdf->Cell(18,8, utf8_decode("Ordem de serviço N° $id_os"),0,0,"C","false");
 
             //Logo da página.
             $pdf->Image("../tools/img/computador-pessoal.png",123,5,20,0);
 
             //Titulo.
-            $pdf->SetFont('OpenSans','', 23);
+            $pdf->SetFont('OpenSansLight','', 23);
             $pdf->SetTextColor(0,0,0);
             $pdf->Cell(75, 33, utf8_decode("Ordem de serviço") ,0,1,'C');
 
@@ -72,71 +72,65 @@
             $pdf->Cell(129,0,'',0,1,'C','true');
 
             //Titulo do section. 
-            $pdf->SetFont('OpenSans','',20);
-            $pdf->SetY(42); $pdf->SetX(15);
+            $pdf->SetFont('OpenSansLight','',20);
+            $pdf->SetY(42); $pdf->SetX(12);
             $pdf->Cell(0,0,utf8_decode("Informações"),0,1,'C');
 
             //Linhas da tabela
             //Dados colocados em linhas
             $pdf->SetY(48); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
+            $pdf->SetFont('OpenSansLight','',12);
             $pdf->SetFillColor(240,248,255);
             $pdf->SetTextColor(0,0,0);
             $pdf->SetDrawColor(220,220,220); //Color da borda dos cells.
-            $pdf->Cell(50,10, utf8_decode('Nome do cliente:'),'B',0,"R","true");
-            $pdf->Cell(90,10, utf8_decode($arraySelectOS['nome_cliente']),'B',0,"L","true");
+            $pdf->Cell(50,10, utf8_decode('Nome do cliente:'),'B',0,"R",true);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->Cell(90,10, utf8_decode($arraySelectOS['nome_cliente']),'B',0,"L",true);
 
-            $pdf->SetY(58); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
-            $pdf->SetFillColor(255,255,255);
-            $pdf->SetTextColor(0,0,0);
-            $pdf->Cell(50,10, utf8_decode('Nome do equipamento:'),'B',0,"R","true");
-            $pdf->Cell(90,10, utf8_decode($arraySelectOS['nome_equipamento']),'B',0,"L","true");
-
-            $pdf->SetY(68); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
+            $pdf->SetY(58.1); $pdf->SetX(5);
             $pdf->SetFillColor(240,248,255);
-            $pdf->SetTextColor(0,0,0);
-            $pdf->Cell(50,10, utf8_decode('Data de recebimento:'),'B',0,"R","true");
-            $pdf->Cell(90,10, utf8_decode(inverteData($arraySelectOS['data_recebimento'])),'B',0,"L","true");
+            $pdf->Cell(50,10, utf8_decode('Nome do equipamento:'),'B',0,"R",true);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->Cell(90,10, utf8_decode($arraySelectOS['nome_equipamento']),'B',0,"L",true);
 
-            $pdf->SetY(79); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
-            $pdf->SetFillColor(255,255,255);
-            $pdf->SetTextColor(0,0,0);
-            $pdf->SetDrawColor(220,220,220); //Color da borda dos cells.
-            $pdf->Cell(50,10, utf8_decode('Data de devolução:'),'B',0,"R","true");
-            $pdf->Cell(90,10, utf8_decode(inverteData($arraySelectOS['data_entrega_cliente'])),'B',0,"L","true");
-
-            $pdf->SetY(89); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
+            $pdf->SetY(68.2); $pdf->SetX(5);
             $pdf->SetFillColor(240,248,255);
-            $pdf->SetTextColor(0,0,0);
-            $pdf->Cell(50,10, utf8_decode('Valor cobrado:'),'B',0,"R","true");
-            $pdf->Cell(90,10, utf8_decode("R$ ".str_replace('.', ',', ($arraySelectOS['valor_reparo']))),'B',0,"L","true");
+            $pdf->Cell(50,10, utf8_decode('Data de recebimento:'),'B',0,"R",true);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->Cell(90,10, utf8_decode(inverteData($arraySelectOS['data_recebimento'])),'B',0,"L",true);
+
+            $pdf->SetY(78.4); $pdf->SetX(5);
+            $pdf->SetFillColor(240,248,255);
+            $pdf->Cell(50,10, utf8_decode('Data de devolução:'),'B',0,"R",true);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->Cell(90,10, utf8_decode(inverteData($arraySelectOS['data_entrega_cliente'])),'B',0,"L",true);
+
+            $pdf->SetY(88.5); $pdf->SetX(5);
+            $pdf->SetFillColor(240,248,255);
+            $pdf->Cell(50,10, utf8_decode('Valor cobrado:'),'B',0,"R",true);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->Cell(90,10, utf8_decode("R$ ".str_replace('.', ',', ($arraySelectOS['valor_reparo']))),'B',0,"L",true);
 
             //Descrições das Ordens de serviço.
-            $pdf->SetY(100); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
-            $pdf->SetFillColor(255,255,255);
-            $pdf->SetTextColor(0,0,0);
+            $pdf->SetY(98.6); $pdf->SetX(5);
+            $pdf->SetFillColor(240,248,255);
             $pdf->Cell(50,35, utf8_decode('Descrição do defeito:'),'B',0,"R","true");
-            $pdf->MultiCell(90, 5, utf8_decode($arraySelectOS['descricao_defeito']),'','J',false);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->MultiCell(90, 5, utf8_decode($arraySelectOS['descricao_defeito']),'B','J',true);
 
             //Descrições das Ordens de serviço.
-            $pdf->SetY(135); $pdf->SetX(5);
-            $pdf->SetFont('OpenSans','',12);
+            $pdf->SetY(133.8); $pdf->SetX(5);
             $pdf->SetFillColor(240,248,255);
-            $pdf->SetTextColor(0,0,0);
             $pdf->Cell(50,35, utf8_decode('Descrição do reparo:'),'B',0,"R","true");
-            $pdf->MultiCell(90, 5, utf8_decode($arraySelectOS['descricao_reparo']),'','J',false);
+            $pdf->SetFillColor(245,245,245);
+            $pdf->MultiCell(90, 5, utf8_decode($arraySelectOS['descricao_reparo']), 'B','J',true);
 
             //Aqui eu exibo a data e hora atual.
-            $pdf->SetFont('OpenSans','',8);
+            $pdf->SetFont('OpenSansLight','',8);
             $pdf->SetFillColor(255,255,255);
             $data_atual = date('d/m/Y - H:i:s');
-            $pdf->SetY(170); $pdf->SetX(88);
-            $pdf->Cell(55,10, utf8_decode("Documento gerado em: $data_atual"),0,0,"C","false");
+            $pdf->SetY(171); $pdf->SetX(88);
+            $pdf->Cell(55,0, utf8_decode("Documento gerado em: $data_atual"),0,0,"C","false");
 
 
             //Aqui eu escolho o tipo de arquivo, e o name dele.
