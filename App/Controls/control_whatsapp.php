6dd8055ef31c5a746f397e9dbefea864f2a335d0
GeovaniPossenti <possentigeovani@gmail.com>
2021-06-05 02:17:15 -0300

    case 'Finalizado'

--- App/Controls/control_whatsapp.php
+++ App/Controls/control_whatsapp.php
@@ -30,7 +30,7 @@
         case 'Orçamento':
             $urlZap = "https://api.whatsapp.com/send?phone=+55$celular_cliente_filtrado&text=Ol%C3%A1%20$primeiroNomeCliente%2C%20tudo%20bem%3F%20Aqui%20%C3%A9%20da%20Matrix%20Inform%C3%A1tica%2C%20e%20viemos%20avisar%20que%20o%20or%C3%A7amento%20de%20reparo%20do%20seu%20equipamento%20ficou%20R%24%20$valorOsDet.";
             break;
-        case 'Entregue':
+        case 'Finalizado':
             $urlZap = "https://api.whatsapp.com/send?phone=+55$celular_cliente_filtrado&text=Ol%C3%A1%20$primeiroNomeCliente%2C%20tudo%20bem%3F%20Aqui%20%C3%A9%20da%20Matrix%20Inform%C3%A1tica%2C%20e%20viemos%20avisar%20que%20o%20reparo%20no%20seu%20equipamento%20j%C3%A1%20foi%20realizado%2C%20basta%20agora%20busca-lo%20na%20loja.%20%F0%9F%98%84";
             break;
         default:
